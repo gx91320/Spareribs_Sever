c2f51c3da0869327c5661383305e3770ec995536
gongxin <18910026892@163.com>
2022-07-19 19:48:54 +0800

    commit

---
+++ spareribs-order/src/main/java/com/spareribs/order/SpareribsOrderApplication.java
@@ -0,0 +1,13 @@
+package com.spareribs.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpareribsOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpareribsOrderApplication.class, args);
+    }
+
+}
