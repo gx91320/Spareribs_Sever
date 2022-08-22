c99b050fef24b2bc7814a6f60bbc2329339e98b7
gongxin <18910026892@163.com>
2022-08-22 21:15:45 +0800

    commit

--- spareribs-user/src/main/java/com/spareribs/user/SpareribsUserApplication.java
+++ spareribs-user/src/main/java/com/spareribs/user/SpareribsUserApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-@MapperScan("com.spareribs.user")
 public class SpareribsUserApplication {
 
     public static void main(String[] args) {
